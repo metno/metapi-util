17e73a13dd189eef4ded337559af84cf3520d7d0
Michael Akinde <michael.akinde@met.no>
2016-08-22 15:12:14 +0200

    Fix.

--- src/main/scala/no/met/geometry/Geometry.scala
+++ src/main/scala/no/met/geometry/Geometry.scala
@@ -29,10 +29,11 @@ import io.swagger.annotations._
 import scala.annotation.meta.field
 
 abstract class Geometry(
-  @(ApiModelProperty @field)(name="@type", value="The type of the geometry object", example="POINT") geomType: String
+  geomType: String
 )
 
 case class Point(
+  @(ApiModelProperty @field)(name="@type", value="The type of the geometry object", example="POINT") geomType: String,
   @(ApiModelProperty @field)(value="Coordinates of the geometry object", example="59.9423, 10.72") coordinates: Seq[Double]
 ) extends Geometry("Point") { 
   
